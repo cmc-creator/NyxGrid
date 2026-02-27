0057102d16cc8e437b934ef9a3e3c7438e87b9f7
Ccooper <ccooper@nyxgrid.io>
2026-02-27 12:30:49 -0700

    fix: disable source maps in prod, add .nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/NyxGrid/',
+  build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 800,
+  },
 })
