7a9a32be6d2b47b2af6c728390ecfa4e5b46d4ca
Ccooper <ccooper@nyxgrid.io>
2026-02-27 12:41:55 -0700

    feat: custom domain nyxgrid.org  CNAME + base path fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NyxGrid/',
+  base: '/',
   build: {
     sourcemap: false,
     chunkSizeWarningLimit: 800,
